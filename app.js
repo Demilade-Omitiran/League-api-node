14a1b81e4bf4f5d41c56eb836e444dbadb4d7473
Demilade-Omitiran <omitirand@gmail.com>
2019-07-13 05:01:29 +0100

    Connect to database

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send("league-api");
 });
+
+mongoose.connect(
+  process.env.DB_CONNECTION_STRING, 
+  { useNewUrlParser: true }, 
+  () => console.log("Connected to db!")
+);
 
 app.listen(8000)
